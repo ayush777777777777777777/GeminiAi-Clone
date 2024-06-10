7765c916891abed9d9e3da67cb496316001323f2
ayush777777777777777777 <166890955+ayush777777777777777777@users.noreply.github.com>
2024-06-10 10:50:21 +0530

    Added my project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,7 +20,7 @@ const Main = () => {
           !showResult ?
             <>
               <div className="greet">
-                <p><span>Hello, Dev.</span></p>
+                <p><span>Hello, User.</span></p>
                 <p>How can I help you today?</p>
               </div>
               <div className="cards">
